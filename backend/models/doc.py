a3a8f3de34dc1fdbff1ad8f7d0b78c295063cb4a
habib610 <habib54562@gmail.com>
2025-11-02 22:45:02 +0600

    model-intialized

---
+++ backend/models/doc.py
@@ -0,0 +1,11 @@
+class Document:
+    id: int
+    title: str
+    description: str
+
+    def __init__(self, id: int, title: str, description: str):
+        self.id  = id
+        self.title = title
+        self.description = description
+        
+
